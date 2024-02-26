730e6d712a3bb91a47e912e71672cebf70188d6d
Jeronmathew44 <156781377+Jeronmathew44@users.noreply.github.com>
2024-02-26 07:32:37 +0530

    Update config.py
    Signed-off-by: Jeronmathew44 <156781377+Jeronmathew44@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
-API_ID = "your_telegram_api_id"
-API_HASH = "your_telegram_api_hash"
-BOT_TOKEN = "your_telegram_bot_token"
+API_ID = "your_actual_telegram_api_id"
+API_HASH = "your_actual_telegram_api_hash"
+BOT_TOKEN = "your_actual_telegram_bot_token"
